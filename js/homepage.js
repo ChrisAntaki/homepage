0cf806b3cb0a0cb7077d5d398a961815aed13653
Chris Antaki <ChrisAntaki@gmail.com>
2016-09-24 22:52:59 -0700

    Refactoring HTML & JavaScript

--- js/homepage.js
+++ js/homepage.js
@@ -1,23 +1,33 @@
-var pool;
+// Particle background
+function ParticleBackground() {
+    this.iosClickTimeout = 0;
+    this.isAnimating = false;
+    this.particleContainer = document.querySelector('.particles');
 
-setTimeout(function() {
-    pool = new Pool();
-    pool.allocate(200);
+    this.addEventListeners();
+    this.createParticles();
+}
+
+ParticleBackground.prototype.addEventListeners = function() {
+    document.body.addEventListener('mousedown', this.onMouseDown.bind(this), false);
+    document.body.addEventListener('touchstart', this.onTouchStart.bind(this), true);
+};
+
+ParticleBackground.prototype.createParticles = function(i) {
     for (var i = 0; i < 100; i++) {
-        createParticle(i);
+        this.createParticle(i);
     }
-}, 50);
+};
 
-var container = document.querySelector('.particles');
-
-function createParticle(i) {
-    var el = pool.pop();
-    var circle = pool.pop();
+ParticleBackground.prototype.createParticle = function(i) {
+    var el = document.createElement('div');
+    el.classList.add('particle');
+    this.particleContainer.appendChild(el);
+    
+    var circle = document.createElement('div');
     circle.classList.add('circle');
     circle.classList.add('animated');
     el.appendChild(circle);
-    el.classList.add('particle');
-    container.appendChild(el);
 
     var column = i % 10 | 0;
     var row = i / 10 | 0;
@@ -30,155 +40,98 @@ function createParticle(i) {
     el.offsetWidth = el.offsetWidth;
     el.style.opacity = 1;
     el.style.transform = 'translate3d(0, 0, 0)';
-}
+};
 
-var iosClickTimeout = 0;
-function onTouchStart(e) {
+ParticleBackground.prototype.onTouchStart = function(e) {
     e.stopPropagation();
-    if (isAnimating) {
+    if (this.isAnimating) {
         return;
     }
-    clearTimeout(iosClickTimeout);
-    iosClickTimeout = setTimeout(onClick.bind(null, e), 100);
 
-    // Polyfill touch event
+    // Debounce
+    clearTimeout(this.iosClickTimeout);
+    this.iosClickTimeout = setTimeout(this.onMouseDown.bind(this, e), 100);
+
+    // Mimic mouse event
     e.clientX = e.touches[0].clientX;
     e.clientY = e.touches[0].clientY;
-}
+};
 
-var isAnimating = false;
-
-function onClick(e) {
-    if (isAnimating) {
+ParticleBackground.prototype.onMouseDown = function(e) {
+    if (this.isAnimating) {
         return;
     }
 
-    var column = e.clientX / window.innerWidth * 10 | 0;
-    var row = e.clientY / window.innerHeight * 10 | 0;
-    var index = row * 10 + column;
-    var node = container.children[index];
+    const column = e.clientX / window.innerWidth * 10 | 0;
+    const row = e.clientY / window.innerHeight * 10 | 0;
+    const index = row * 10 + column;
+    const node = this.particleContainer.children[index];
 
-    isAnimating = true;
-    createWave(node.meta.column, node.meta.row);
-    setTimeout(onReadyToClean, 1500);
-}
+    this.isAnimating = true;
+    this.createWave(node.meta.column, node.meta.row);
+    setTimeout(this.cleanParticles.bind(this), 1500);
+};
 
-function onReadyToClean() {
-    cleanParticles();
-    isAnimating = false;
-}
-
-function cleanParticles() {
-    for (var i = 0; i < container.children.length; i++) {
-        var node = container.children[i].firstElementChild;
+ParticleBackground.prototype.cleanParticles = function() {
+    for (let i = 0; i < this.particleContainer.children.length; i++) {
+        const node = this.particleContainer.children[i].firstElementChild;
         node.style.animationName = '';
     }
-}
 
-function positionIsValid(column, row) {
+    this.isAnimating = false;
+};
+
+ParticleBackground.prototype.positionIsValid = function(column, row) {
     return !(
         column < 0 || column > 9
         ||
         row < 0 || row > 9
     );
-}
+};
 
-function animateParticle(column, row, delay) {
-    var index = row * 10 + column;
-    
-    var node = container.children[index].firstElementChild;
+ParticleBackground.prototype.animateParticle = function(column, row, delay) {
+    const index = row * 10 + column;
+    const node = this.particleContainer.children[index].firstElementChild;
     node.style.animationName = 'rubberBand';
     node.style.animationDelay = delay * 100 + 'ms';
-}
+};
 
-function createWave(column, row) {
-    animateParticle(column, row, 0);
-    for (var distance = 1; distance <= 9; distance++) {
+ParticleBackground.prototype.createWave = function(column, row) {
+    this.animateParticle(column, row, 0);
+    for (let distance = 1; distance <= 9; distance++) {
         // Top left to right
-        for (var i = column - distance; i < column + distance; i++) {
-            if (!positionIsValid(i, row - distance)) {
+        for (let i = column - distance; i < column + distance; i++) {
+            if (!this.positionIsValid(i, row - distance)) {
                 continue;
             }
-            animateParticle(i, row - distance, distance);
+            this.animateParticle(i, row - distance, distance);
         }
 
         // Top right to bottom
-        for (var i = row - distance; i < row + distance; i++) {
-            if (!positionIsValid(column + distance, i)) {
+        for (let i = row - distance; i < row + distance; i++) {
+            if (!this.positionIsValid(column + distance, i)) {
                 continue;
             }
-            animateParticle(column + distance, i, distance);
+            this.animateParticle(column + distance, i, distance);
         }
 
         // Bottom right to left
-        for (var i = column + distance; i > column - distance; i--) {
-            if (!positionIsValid(i, row + distance)) {
+        for (let i = column + distance; i > column - distance; i--) {
+            if (!this.positionIsValid(i, row + distance)) {
                 continue;
             }
-            animateParticle(i, row + distance, distance);
+            this.animateParticle(i, row + distance, distance);
         }
 
         // Bottom left to top
-        for (var i = row + distance; i > row - distance; i--) {
-            if (!positionIsValid(column - distance, i)) {
+        for (let i = row + distance; i > row - distance; i--) {
+            if (!this.positionIsValid(column - distance, i)) {
                 continue;
             }
-            animateParticle(column - distance, i, distance);
+            this.animateParticle(column - distance, i, distance);
         }
     }
-}
+};
 
-document.body.addEventListener('touchstart', onTouchStart, true);
-document.body.addEventListener('mousedown', onClick, false);
-
-class Pool {
-    constructor(params) {
-        this.storage = [];
-        if (params) {
-            if (params.tagName) {
-                this.tagName = params.tagName.toLowerCase();
-            }
-            if (params.namespace) {
-                this.namespace = params.namespace;
-            }
-        }
-    }
-
-    push(el) {
-        if (el.tagName.toLowerCase() !== this.tagName) {
-            return;
-        }
-        
-        this.storage.push(el);
-    }
-
-    pop() {
-        if (this.storage.length === 0) {
-            return this.create();
-        } else {
-            return this.storage.pop();
-        }
-    }
-
-    create() {
-        if (this.namespace) {
-            return document.createElementNS(this.namespace, this.tagName);
-        } else {
-            return document.createElement(this.tagName);
-        }
-    }
-
-    allocate(size) {
-        if (this.storage.length >= size) {
-            return;
-        }
-
-        var difference = size - this.storage.length;
-        for (var i = 0; i < difference; i++) {
-            this.storage.push(this.create());
-        }
-    }
-}
-
-Pool.prototype.namespace = '';
-Pool.prototype.tagName = 'div';
+// Create background
+const background = new ParticleBackground();
